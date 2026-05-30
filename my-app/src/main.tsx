f017b388b5d3d29c23eaa8edf15e30751d2fe89f
Liridona Kurrumeli <kurrumelil@gmail.com>
2026-05-31 00:10:26 +0200

    fix errors

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,24 +1,10 @@
-import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { AuthProvider } from "./context/AuthContext";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>,
-);
-
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
